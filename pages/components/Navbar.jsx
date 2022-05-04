d5e0a6197991d87f89b39dd4860bf3b683c1cf7a
jmsmf28 <jmsmf2011@gmail.com>
2022-05-04 05:53:53 +0100

    Initial commit from Create Next App

---
+++ pages/components/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Navbar() {
+  return (
+    <div className="flex justify-end bg-slate-300">
+      <span>+351 926415585 </span>
+      <span>| Rua Padre Costa 1132 S. Mamede de Infesta |</span>
+      <span> fernandes648@gmail.com</span>
+    </div>
+  );
+}
+
+export default Navbar;
